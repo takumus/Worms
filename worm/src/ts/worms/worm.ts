8cd5bed510cb9c186961a66a158f7f9f1bf7fc93
takumus <me@takumus.com>
2017-02-03 14:28:54 +0900

    等速運動を実現するためのメソッドaddStep完成

--- worm/src/ts/worms/worm.ts
+++ worm/src/ts/worms/worm.ts
@@ -5,6 +5,7 @@ export default class Worm extends PIXI.Graphics{
     protected length:number;
     private line:Line;
     private routeIndex:number;
+    private step:number;
     constructor(length:number){
         super();
         this.length = Math.floor(length);
@@ -36,14 +37,19 @@ export default class Worm extends PIXI.Graphics{
         if(line.getLength() < this.length) return;
         this.line = line;
     }
-    public setStep(pos:number):void{
-        if(!this.line) return;
-        if(pos < 0) pos = 0;
-        if(pos > 1) pos = 1;
+    public addStep(step:number):boolean{
+        const p = 1 / this.line.getLength() * step;
+        return this.setStep(this.step + p);
+    }
+    public setStep(step:number):boolean{
+        if(step < 0) step = 0;
+        if(step > 1) step = 1;
+        this.step = step;
+        if(!this.line) return false;
         const beginIndex = this.length - 1;
         const length = this.line.getLength() - beginIndex - 1;
-        const posIndex = Math.floor(length * pos);
-        const offset = (length * pos - posIndex);
+        const posIndex = Math.floor(length * step);
+        const offset = (length * step - posIndex);
         for(let i = 0; i < this.bone.getLength(); i ++){
             const id = beginIndex-i + posIndex;
             const b = this.bone.at(i);
@@ -59,6 +65,10 @@ export default class Worm extends PIXI.Graphics{
             b.x = l.x + dx;
             b.y = l.y + dy;
         }
+        return true;
+    }
+    public getStep():number{
+        return this.step;
     }
     public reverse():void{
         this.bone.reverse();
