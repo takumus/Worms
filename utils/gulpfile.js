718662d55b503a7918d7a7ee4d4380341a532b6e
takumus <me@takumus.com>
2017-02-04 13:52:18 +0900

    gulpfile間違え修正

--- utils/gulpfile.js
+++ utils/gulpfile.js
@@ -3,13 +3,13 @@ var webpack = require('gulp-webpack');;
 var webpackConfig = require('./webpack.config.js');
  
 gulp.task('webpack', function () {
-    gulp.src(['./routes/*.ts'])
+    gulp.src(['./src/*.ts'])
     .pipe(webpack(webpackConfig))
     .pipe(gulp.dest('.'));
 });
  
 gulp.task('watch', function () {
-    gulp.watch('./routes/**/*.ts', ['webpack']);
+    gulp.watch('./src/**/*.ts', ['webpack']);
 });
  
 gulp.task('default', ['webpack','watch']);
