1e895b78942e581c10c5e3d176def7251e5c8216
takumus <me@takumus.com>
2017-02-09 17:56:06 +0900

    created 'Nasty2'

---
+++ worms/src/nasty2.ts
@@ -0,0 +1,73 @@
+///<reference path="@base.ts" />
+namespace WORMS{
+    export class Nasty2 extends Base{
+        private thickness:number;
+        private body:Array<BodyPos>;
+        private colors:{fill:number, border:number};
+        constructor(length:number, thickness:number, fillColor:number = 0xffffff, borderColor:number = 0x000000){
+            super(length);
+            this.thickness = thickness/2;
+            this.body = [];
+            for(let i = 0; i < length; i ++){
+                this.body.push(new BodyPos());
+            }
+            this.setColor(fillColor, borderColor);
+        }
+        public setColor(fillColor:number, borderColor:number):void{
+            this.colors = {
+                fill : fillColor,
+                border : borderColor
+            }
+        }
+        public render(){
+            const bbone = this.bone.at(0);
+            //ワームの外殻を生成
+            const ebone = this.bone.at(this.bone.getLength() - 1);
+            const bbody = this.body[0];
+            const ebody = this.body[this.body.length - 1];
+            bbody.left.x = bbone.x;
+            bbody.left.y = bbone.y;
+            ebody.left.x = ebone.x;
+            ebody.left.y = ebone.y;
+            
+            this.clear();
+            this.lineStyle(3, this.colors.border);
+            this.beginFill(this.colors.fill);
+            this.moveTo(bbody.left.x, bbody.left.y);
+            for(let i = 1; i < this.body.length; i ++){
+                this.lineTo(this.body[i].left.x, this.body[i].left.y);
+            }
+            for(let i = this.body.length - 2; i >= 2; i --){
+                this.lineTo(this.body[i].right.x, this.body[i].right.y);
+            }
+            this.lineTo(bbody.left.x, bbody.left.y);
+            this.endFill();
+        }
+        private generateCovor(body:Array<BodyPos>, begin:number, end:number, ):void{
+            const L = end - begin;
+            for(let i = begin; i < L; i ++){
+                const nbone = this.bone.at(i);
+                const nbody = this.body[i];
+                let vx = this.bone.at(i-1).x - nbone.x;
+                let vy = this.bone.at(i-1).y - nbone.y;
+                const r = Math.sin(i/L*(Matthew.H_PI))*this.thickness;
+                const vl = vx*vx+vy*vy;
+                const vr = Math.sqrt(vl);
+                vx = vx / vr * r;
+                vy = vy / vr * r;
+                nbody.left.x = nbone.x + -vy;
+                nbody.left.y = nbone.y + vx;
+                nbody.right.x = nbone.x + vy;
+                nbody.right.y = nbone.y + -vx;
+            }
+        }
+    }
+    class BodyPos{
+        public left:UTILS.Pos;
+        public right:UTILS.Pos;
+        constructor(){
+            this.left = new UTILS.Pos();
+            this.right = new UTILS.Pos();
+        }
+    }
+}
